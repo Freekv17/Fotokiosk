8bcf9d72346b5543773866be5ddbdb2558551733
uitgeteld <swendewit07@gmail.com>
2025-05-07 08:32:34 +0200

    basic station selector

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Server=localhost;Database=nsdatabase;Uid=root;Pwd=;";
+
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            bool stationSelected = false;
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM netherlands_train_stations;";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (stationSelected == false)
+                {
+                    Console.WriteLine("Kies uit op welk station je staat.");
+                    string selectStation = Console.ReadLine();
+                    Console.Clear();
+
+                    while (reader.Read())
+                    {
+                        string stationName = reader["name"].ToString();
+                        //Console.WriteLine(stationName);
+
+
+                        // Optional comparison
+                        if (selectStation.ToLower() == stationName.ToLower())
+                        {
+                            Console.WriteLine("Station selected: " + stationName);
+                            stationSelected = true;
+                            reader.Close();
+                            conn.Close();
+                            break;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            while (stationSelected)
+            {
+                //Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NSApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NSApp")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cfc5f6b6-5ff2-4240-9d40-282e44781a9f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
