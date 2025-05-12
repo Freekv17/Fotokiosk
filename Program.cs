c4bfe8ce6b212a73079b12374c8e90ba75ae9d65
uitgeteld <swendewit07@gmail.com>
2025-05-12 09:37:46 +0200

    duplicate
    removed a duplicate

--- Program.cs
+++ Program.cs
@@ -41,10 +41,6 @@ class Program
                         Console.WriteLine("Error: Station " + selectStation + " bestaat niet, probeer het opnieuw.");
                     }
 
-                    if (!stationSelected)
-                    {
-                        Console.WriteLine("Error: Station " + selectStation + " bestaat niet, probeer het opnieuw.");
-                    }
                 }
                 
             }
