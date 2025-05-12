09c6a565459b3e49939f2ca836334b14e20e1c51
WesselBiemans <119673590+WesselBiemans@users.noreply.github.com>
2025-05-12 09:39:57 +0200

    Admin programma start
    Zoekt netzoals enquete eerst voor station

---
+++ admin/Program.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string connectionString = "Server=localhost;Database=nsdatabase;Uid=root;Pwd=;";
+
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            bool stationSelected = false;
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM netherlands_train_stations;";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (stationSelected == false)
+                {
+                    Console.WriteLine("Kies op welk station je staat.");
+                    string selectStation = Console.ReadLine();
+                    Console.Clear();
+
+                    while (reader.Read())
+                    {
+                        string stationName = reader["name"].ToString();
+     
+                        if (selectStation.ToLower() == stationName.ToLower())
+                        {
+                            Console.WriteLine("Station geselecteerd: " + stationName);
+                            stationSelected = true;
+                            reader.Close();
+                            break;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                        }
+                    }
+
+                    if (!stationSelected)
+                    {
+                        Console.WriteLine("Error: Station " + selectStation + " bestaat niet, probeer het opnieuw.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            conn.Close();
+        }
+    }
+}
